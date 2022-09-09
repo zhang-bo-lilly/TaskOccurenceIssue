00f319aa7aee84c12999432dd0af6cee91144e7f
Bo Zhang <zhang_bo3@lilly.com>
2022-09-08 21:35:29 -0400

    Initial Commit

---
+++ TaskOccurenceIssue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskOccurenceIssue
+//
+//  Created by Bo Zhang on 9/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskOccurenceIssue/TaskOccurenceIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskOccurenceIssueApp.swift
+//  TaskOccurenceIssue
+//
+//  Created by Bo Zhang on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskOccurenceIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
